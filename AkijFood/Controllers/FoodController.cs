0424c5ae4e733dfa7b77663a3c0772b114569da4
hassanul50 <hasanakib27@gmail.com>
2021-07-04 00:01:36 +0600

    code for assignment

---
+++ AkijFood/Controllers/FoodController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AkijFood.Models;
+using Microsoft.EntityFrameworkCore;
+using static AkijFood.Models.FoodClass;
+
+namespace AkijFood.Controllers
+{
+    public class FoodController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public FoodController(ApplicationDbContext db)
+        {
+            _db = db;    
+        }
+
+        public IActionResult Index()
+        {
+            var displayData = _db.tblColdDrinks.ToList();
+            return View(displayData);
+        }
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(FoodClass nec)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(nec);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nec);
+
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getempdetails = await _db.tblColdDrinks.FindAsync(id);
+            return View(getempdetails);
+        }
+        [HttpPost]
+        public async Task<IActionResult>Edit(FoodClass nc)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(nc);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(nc);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var getempdetails = await _db.tblColdDrinks.FindAsync(id);
+            return View(getempdetails);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            
+            var getempdetails = await _db.tblColdDrinks.FindAsync(id);
+            _db.tblColdDrinks.Remove(getempdetails);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+
+            return View(getempdetails);
+        }
+
+
+        
+
+        
+    }
+}
---
+++ AkijFood/Models/FoodClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AkijFood.Models
+{
+    public class FoodClass
+    {
+        [Key]
+        public int IntColdDrinkId { get; set; }
+
+        [Required(ErrorMessage ="enter cold drinks name")]
+        [Display(Name = "Drinks Name")]
+
+        public string StrColdDrinksName { get; set; }
+
+        [Required(ErrorMessage ="enter quantity")]
+        [Display(Name = "Quantity")]
+
+        public decimal NumQuantity { get; set; }
+
+        [Required(ErrorMessage ="Enter price of the drinks")]
+        [Display(Name = "Unit price")]
+
+        public decimal NumUnitPrice { get; set; }
+
+        public class nec
+        {
+        }
+    }
+}
